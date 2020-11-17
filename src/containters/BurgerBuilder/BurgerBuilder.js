9859687daa4dd5c3b095c63f803a776394e55494
Dang Chi <chid@jobready.com.au>
2020-11-17 10:55:36 +0700

    Handling Purchases and Updating the UI

--- src/containters/BurgerBuilder/BurgerBuilder.js
+++ src/containters/BurgerBuilder/BurgerBuilder.js
@@ -7,7 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import WithErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
-import * as burgerBuilderActions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
 class BurgerBuilder extends Component{
@@ -39,6 +39,8 @@ class BurgerBuilder extends Component{
   }
 
   purchasContinueHandler = () => {
+    console.log('purchasContinueHandler')
+    this.props.onInitPurchase();
     this.props.history.push('/checkout');
   }
 
@@ -96,9 +98,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-    onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
-    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
+    onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+    onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(actions.initIngredients()),
+    onInitPurchase: () => dispatch(actions.purchaseInit())
   }
 }
 
